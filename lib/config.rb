4367b7a7e01a5597b6992fa752906e1af60dcc37
Nick Barth <nickbarth@live.com>
2012-11-17 02:40:24 +0000

    added activerecord connection

--- lib/config.rb
+++ lib/config.rb
@@ -1,8 +1,21 @@
 require 'bundler'
 Bundler.require
 
+require 'logger'
+require './db/connection'
+
 class SinatraApp < Sinatra::Base
+  before { ActiveRecord::Base.verify_active_connections! }
+  after  { ActiveRecord::Base.clear_active_connections! }
+
+  configure :development do
+    use SprockAssets
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+  end
+
   configure do
+    enable :sessions
+    use Rack::Flash
     set :views, 'app/views'
   end
 end
