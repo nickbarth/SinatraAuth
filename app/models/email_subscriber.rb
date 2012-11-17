e5457b69bf9e501844963209c325be53470c23ab
Nick Barth <nickbarth@live.com>
2012-11-17 17:10:27 +0000

    removed instance logging

--- app/models/email_subscriber.rb
+++ app/models/email_subscriber.rb
@@ -1,19 +1,18 @@
 class EmailSubscriber
   attr_accessor :user
-  attr_accessor :logger
 
   # Inject the user class.
   def initialize(user)
     @user = user
-    @logger = Logger.new(STDOUT)
   end
 
   # Asynchronously queue a welcome email for a user.
   def welcome_email
-    logger.debug 'User welcome email sent.'
+    # User welcome email sent.
   end
+
   # Asynchronously queue a password reset email for a user.
   def reset_email
-    logger.debug 'User reset email sent.'
+    # User reset email sent.
   end
 end
